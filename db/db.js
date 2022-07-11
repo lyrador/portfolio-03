e6531785a000e513cf837f5e6bbea670a55dbe5d
lyrador <yeedaryl700@gmail.com>
2022-07-11 11:14:30 +0800

    Initial commit

---
+++ db/db.js
@@ -0,0 +1,268 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Hey there! &#128075;",
+				"I'm Daryl. I’m currently a Penultimate Year Information Systems Undergraduate at the National University of Singapore.",
+				"I'm a developer, passionate in integrating IT solutions into businesses with a strong desire to learn, having a broad scope of interests ranging from game development and music to content creation. Given my technical background, leadership experience and innovative mindset, I believe I would be a great team player and contribute significantly to projects, making me a great asset to any company. .",
+				"I am currently looking for opportunities to further hone my skills and explore the different development roles in the realm of computing.",
+			],
+		},
+		contact: {
+			text: [
+				"If you would like to get in touch with me, feel free to send me an email at darylyee.jh@gmail.com",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Languages",
+			skillName: "JavaScript, Java, Lua",
+			color: "1",
+			percentage: "90",
+		},
+		{
+			title: "Frameworks/Libraries",
+			skillName: "Angular, ReactJS",
+			color: "6",
+			percentage: "80",
+		},
+		{
+			title: "Backend",
+			skillName: "Nodejs, MongoDB",
+			color: "2",
+			percentage: "40",
+		},
+		// {
+		// 	title: "Clouds",
+		// 	skillName: "AWS(EC2, S3), Heroku, Netlify",
+		// 	color: "3",
+		// 	percentage: "30",
+		// },
+		{
+			title: "Design",
+			skillName: "HTML, Bootstrap, SCSS, CSS",
+			color: "4",
+			percentage: "70",
+		},
+		{
+			title: "Version Control",
+			skillName: "GitHub",
+			color: "7",
+			percentage: "80",
+		},
+		// {
+		// 	title: "Tools",
+		// 	skillName: "Postman, Chrome DevTools",
+		// 	color: "3",
+		// 	percentage: "80",
+		// },
+		{
+			title: "Editor",
+			skillName: "VS Code, Netbeans, IntelliJ",
+			color: "6",
+			percentage: "70",
+		},
+	],
+	projects: {
+		web: [
+			{
+				projectName: "NUS Voices Website",
+				image: "images/chongjianshuguang-thumbnail.jpg",
+				summary:
+					"Developed a front end website for my recreational mandopop club to provide more information about the club",
+				preview: "https://nusvoices.com/",
+				techStack: [
+					"ReactJS",
+					"Bootstrap",
+					"JavaScript",
+					"Netlify",
+				],
+			},
+		],
+		software: [
+			{
+				projectName: "NUS Society",
+				image: "images/nusociety.png",
+				summary:
+					"NUSociety is an all-in-one co-curricular activities portal for NUS students with management capabilities, providing a one-stop platform for students to find information about societies and events, sign up for them, network and communicate within and across organisations.",
+				preview:
+					"https://github.com/lyrador",
+				techStack: ["Angular", "JSF", "JavaScript", "Glassfish"],
+			},
+			// {
+			// 	projectName: "WhatsApp-Bot",
+			// 	image: "images/whatsappbot.jpg",
+			// 	summary:
+			// 		"Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
+			// 	preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
+			// 	techStack: ["Selenium", "Chrome Webdriver", "Python"],
+			// },
+			// {
+			// 	projectName: "Bill Generator",
+			// 	image: "images/billgenerator.png",
+			// 	summary:
+			// 		"GUI to transfer data to excel sheets and generate bills on the local shops.",
+			// 	preview: "https://github.com/vinaysomawat/Bill-Generator",
+			// 	techStack: ["Tkinter", "Openxlpy", "Python"],
+			// },
+		],
+		android: [
+			// {
+			// 	projectName: "NITW-CSE",
+			// 	image: "images/nitwcse.jpg",
+			// 	summary:
+			// 		"The Application display details of Department courses, reference books, research, publication and faculty profile.",
+			// 	preview: "https://github.com/vinaysomawat/NITW-CSE",
+			// 	techStack: ["JAVA", "XML", "Android"],
+			// },
+			// {
+			// 	projectName: "CareerHigh-App",
+			// 	image: "images/carrerhigh.png",
+			// 	summary:
+			// 		"The Application display the webpages of website careerhigh.in in android devices.",
+			// 	preview: "https://github.com/vinaysomawat/CareerHigh-Android",
+			// 	techStack: ["JAVA", "XML", "Android"],
+			// },
+		],
+		freelance: [
+			// {
+			// 	projectName: "SnylloAir.com",
+			// 	image: "images/snylloair.png",
+			// 	summary:
+			// 		"Developed a company website to showcase the purpose, services and products provided by the company to audience.",
+			// 	preview: "https://www.snylloair.com/",
+			// 	techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
+			// },
+			// {
+			// 	projectName: "Delivery+",
+			// 	image: "images/AM-Logo-.png",
+			// 	summary:
+			// 		"Android Application to display website in android devices.",
+			// 	preview:
+			// 		"https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+			// 	techStack: ["Android", "JAVA", "Play Store"],
+			// },
+		],
+	},
+	experience: [
+		{
+			title: "NCS Pte Ltd",
+			duration: "May 2022 - Present",
+			subtitle: "Software Engineer Intern",
+			details: [
+				"Full stack development with Angular and Spring."
+			],
+			tags: [
+				"JavaScript",
+				"Angular",
+				"Bootstrap",
+				"Spring",
+				"Oracle",
+				"SQL",
+			],
+			icon: "heartbeat",
+		},
+		{
+			title: "Ottodot",
+			duration: "May 2021 - August 2021",
+			subtitle: "Game Developer Intern",
+			details: [
+				"Utilised Lua to create a new EdTech game called Ottoworld: Parkour Rangers, with the Alpha Version released in July 2021, currently at 1 million+ total visits.",
+				"Collaborated with and co-developed games with international developers and designers.",
+				"Provided suggestions and presented new ideas to the management as well as improved on and debugged games.",
+				"Designed Unified Modelling Language (UML) diagrams to assist in code organisation and structuring.",
+				"Analysed data obtained from GameAnalytics and Amazon Web Services (AWS) to make plans for future installments.",
+				"Facilitated game testing sessions with 10+ children across the globe.",
+			],
+			tags: [
+				"Lua",
+			],
+			icon: "qrcode",
+		},
+	],
+	education: [
+		{
+			title: "Bachelors in Information Systems with Honours",
+			duration: "",
+			subtitle: "National Univerisity of Singapore, Singapore",
+			details: [
+				"-",
+				"-",
+				"-",
+			],
+			tags: [
+				"Data Structures & Algorithms",
+				"Database Management System",
+				"Computer Networks",
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "Singapore-Cambridge GCE Advanced Level",
+			duration: "",
+			subtitle: "Victoria Junior College, Singapore",
+			details: [
+				"Secured 83.75/90 RP in GCE Advanced Level Examinations.",
+				"Attained multiple awards, such as the Edusave Awards for Achievement, Good Leadership and Service, which is awarded to recognize pupil's leadership qualities, community services rendered and excellence in non-academic activities, as well as Gold Distinction Award for Guitar Ensemble",
+			],
+			tags: ["Biology", "Chemistry", "Mathematics", "Economics"],
+			icon: "book",
+		},
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				{
+					text: "Stackoverflow",
+					link: "https://stackoverflow.com",
+				},
+				{
+					text: "GitHub",
+					link: "https://github.com/lyrador",
+				},
+				{
+					text: "LeetCode",
+					link: "https://leetcode.com",
+				},
+			],
+		},
+		{
+			label: "Resources",
+			data: [
+				{
+					text: "Enable Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				{
+					text: "Print this page",
+					func: "window.print()",
+				},
+				{
+					text: "Clone this page",
+					link: "https://github.com/lyrador/darylyeejh.github.io",
+				},
+			],
+		},
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.linkedin.com/in/darylyee/",
+				},
+				{
+					text: "Twitter",
+					link: "https://twitter.com/",
+				},
+			],
+		},
+		{
+			label: "copyright-text",
+			data: [
+				"-",
+			],
+		},
+	],
+};
